7ec36e1995d73e81e7e69d2bca31493b60da55c0
Dariusz Tomaszewski <linuxiarz1624pl@gmail.com>
2018-03-28 20:38:50 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 28 mar 2018
+ *      Author: darek
+ */
+#include <iostream>
+
+int main(int argc, char **argv) {
+	std::cout << "Własny język programowania" << std::endl;
+	return 0;
+}
